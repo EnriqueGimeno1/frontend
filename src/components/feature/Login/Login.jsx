d5e3c1d2905c0e247b3a9cfa12152e06ee646331
Enrique Gimeno <enriquejgimeno@gmail.com>
2022-11-04 05:34:20 -0300

    Add Login component body

--- src/components/feature/Login/Login.jsx
+++ src/components/feature/Login/Login.jsx
@@ -1,5 +1,28 @@
 import React from "react";
+import "./Login.css";
 
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <div className="cantainer">
+      <div className="login-cantainer">
+        <img
+          src="https://media.istockphoto.com/photos/english-bulldog-in-a-pumpkin-wagon-picture-id1280623255?b=1&k=20&m=1280623255&s=612x612&w=0&h=KzVaFbeq9HJdQZdGPXNd5xbfPUbKsjvhZdNC6Ii92pM="
+          className="logo"
+          // src=""
+          alt="Logo"
+        />
+        <form action="" className="login-form">
+          <label htmlFor="user" className="form-label">
+            Usuario
+          </label>
+          <input type="text" className="form-input" name="User" />
+          <label htmlFor="password" className="form-label">
+            Contraseña
+          </label>
+          <input type="text" className="form-input" name="password" />
+          <input type="submit" value="Ingresar" />
+        </form>
+      </div>
+    </div>
+  );
 }
