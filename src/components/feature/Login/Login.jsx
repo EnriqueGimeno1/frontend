b9ca593903bd3e3a5d3d41af8cd046cc07ebb981
Enrique Gimeno <enriquejgimeno@gmail.com>
2022-11-04 06:34:33 -0300

    Add placeholder text and class names to login component

--- src/components/feature/Login/Login.jsx
+++ src/components/feature/Login/Login.jsx
@@ -3,8 +3,8 @@ import "./Login.css";
 
 export default function Login() {
   return (
-    <div className="cantainer">
-      <div className="login-cantainer">
+    <div className="container">
+      <div className="login-container">
         <img
           src="https://media.istockphoto.com/photos/english-bulldog-in-a-pumpkin-wagon-picture-id1280623255?b=1&k=20&m=1280623255&s=612x612&w=0&h=KzVaFbeq9HJdQZdGPXNd5xbfPUbKsjvhZdNC6Ii92pM="
           className="logo"
@@ -15,12 +15,22 @@ export default function Login() {
           <label htmlFor="user" className="form-label">
             Usuario
           </label>
-          <input type="text" className="form-input" name="User" />
+          <input
+            type="text"
+            className="form-input"
+            name="User"
+            placeholder="Usuario"
+          />
           <label htmlFor="password" className="form-label">
             Contraseña
           </label>
-          <input type="text" className="form-input" name="password" />
-          <input type="submit" value="Ingresar" />
+          <input
+            type="password"
+            className="form-input"
+            name="password"
+            placeholder="Contraseña"
+          />
+          <input className="submit-button" type="submit" value="Ingresar" />
         </form>
       </div>
     </div>
