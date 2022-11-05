1af01a8afca563638866dac3a4fec36f092541c7
Enrique Gimeno <enriquejgimeno@gmail.com>
2022-11-05 04:24:54 -0300

    Add AdminPanel to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Login from "./components/feature/Login/Login";
+import AdminPanel from "./components/feature/AdminPanel/AdminPanel";
 
 function App() {
   return (
     <div className="App">
       <Login />
+      <AdminPanel />
     </div>
   );
 }
