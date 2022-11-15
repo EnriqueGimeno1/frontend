79e2664601af444b4b2583385261104a96222b5e
Enrique Gimeno <enriquejgimeno@gmail.com>
2022-11-15 11:23:08 -0300

    Set CreateUser as Default component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Login from "./components/feature/Login/Login";
 import AdminPanel from "./components/feature/AdminPanel/AdminPanel";
+import { CreateUser } from "./components/feature/forms/CreateUser";
 
 function App() {
   return (
     <div className="App">
-      <Login />
-      <AdminPanel />
+      {/* <Login />
+      <AdminPanel /> */}
+      <CreateUser />
     </div>
   );
 }
