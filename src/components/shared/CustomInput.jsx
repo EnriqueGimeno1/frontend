febd3c105b33e80893820ab12db2bcd2ab96f1f7
Enrique Gimeno <enriquejgimeno@gmail.com>
2022-11-15 11:24:46 -0300

    Add Inicial number validation to CustomInput

--- src/components/shared/CustomInput.jsx
+++ src/components/shared/CustomInput.jsx
@@ -1,12 +1,26 @@
 import { ErrorMessage, useField } from "formik";
+import { useState } from "react";
 
 export const CustomInput = ({ label, ...props }) => {
-  const [field] = useField(props);
-
+  const [field, , helper] = useField(props);
+  const [userInput, setUserInput] = useState();
+  const acceptOnlyNumbers = () => {
+    helper.setValue(field.value);
+    if (field.value >= 0) {
+      helper.setValue(field.value);
+    }
+    console.log(field.value);
+  };
   return (
     <>
       <label htmlFor={props.id || props.name}>{label}</label>
-      <input className="text-input" {...field} {...props} />
+      <input
+        className="text-input"
+        {...field}
+        {...props}
+        value={userInput}
+        onChange={acceptOnlyNumbers}
+      />
       <ErrorMessage
         name={props.name}
         component="span"
