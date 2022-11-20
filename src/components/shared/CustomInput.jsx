f514ef651e784d36f54d9f4645b57802ada7e78a
Luis Martinez <ernestox11@hotmail.com>
2022-11-20 09:38:12 -0400

    "allowedCharacters" prop to restrict characters that can be typed in an Input

--- src/components/shared/CustomInput.jsx
+++ src/components/shared/CustomInput.jsx
@@ -2,30 +2,59 @@ import { ErrorMessage, useField } from "formik";
 import { useState } from "react";
 
 export const CustomInput = ({ label, ...props }) => {
-  const [field, , helper] = useField(props);
-  const [userInput, setUserInput] = useState();
-  const acceptOnlyNumbers = () => {
-    helper.setValue(field.value);
-    if (field.value >= 0) {
-      helper.setValue(field.value);
-    }
-    console.log(field.value);
-  };
-  return (
-    <>
-      <label htmlFor={props.id || props.name}>{label}</label>
-      <input
-        className="text-input"
-        {...field}
-        {...props}
-        // value={userInput}
-        // onChange={acceptOnlyNumbers}
-      />
-      <ErrorMessage
-        name={props.name}
-        component="span"
-        className="custon-span-error-class"
-      />
-    </>
-  );
+	const [field, , helper] = useField(props);
+
+	const handleInputChange = (event) => {
+		// Disable default event handler
+		event.preventDefault();
+
+		// Extract current Input value
+		const { value } = event.target;
+
+		// Display current Input value
+		// console.log(value);
+
+		// VALIDATION - Verifying for character restrictions
+		if (props.allowedCharacters) {
+			// Verifying if the field value changed
+
+			switch (props.allowedCharacters) {
+				// CASE: Positive integers only
+				case "positiveIntegers": {
+					const regex = /^[1-9]\d*$/;
+					if (regex.test(value.toString()) || value === "") {
+						helper.setValue(value);
+					}
+					break;
+				}
+
+				// CASE: Letters only
+
+				default: {
+					break;
+				}
+			}
+		} else {
+			// Default behavior when the 'allowedCharacters' prop is not received
+			helper.setValue(value);
+		}
+	};
+
+	return (
+		<>
+			<label htmlFor={props.id || props.name}>{label}</label>
+			<input
+				className="text-input"
+				{...field}
+				{...props}
+				// value={userInput}
+				onChange={handleInputChange}
+			/>
+			<ErrorMessage
+				name={props.name}
+				component="span"
+				className="custon-span-error-class"
+			/>
+		</>
+	);
 };
