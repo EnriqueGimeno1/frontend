f24c7e97a001cac420d041b9eebd81d9963bee9c
Enrique Gimeno <enriquejgimeno@gmail.com>
2022-11-21 15:53:29 -0300

    Adjust CustomInput and CreateUser attributes

--- src/components/feature/forms/CreateUser.jsx
+++ src/components/feature/forms/CreateUser.jsx
@@ -5,134 +5,147 @@ import "./FormsStyles.css";
 import { CustomSelect, CustomInput } from "../../shared";
 
 export const CreateUser = () => {
-	return (
-		<div>
-			<h1>Create User</h1>
+  return (
+    <div>
+      <h1>Create User</h1>
 
-			<Formik
-				initialValues={{
-					firstName: "",
-					lastName: "",
-					email: "",
-					password: "",
-					isActive: true,
-					branchOffice: "",
-					accessLevel: "",
-					nationality: "",
-					idNumber: "",
-					phoneNumber: "",
-					driverLicenseStatus: "",
-					designatedVehicleId: "",
-				}}
-				onSubmit={(values) => {
-					console.log(values);
-				}}
-				validationSchema={Yup.object({
-					firstName: Yup.string()
-						.max(15, "Debe de tener 15 caracteres o menos")
-						.required("Requerido"),
-					lastName: Yup.string()
-						.max(15, "Debe de tener 15 caracteres o menos")
-						.required("Requerido"),
-					email: Yup.string()
-						.email("El correo no tiene un formato válido")
-						.required("Requerido"),
-					password: Yup.string()
-						.required("Por favor escriba su contraseña")
-						.min(8, "Debe contener al menos 8 caracteres")
-						.max(20, "Debe contener un maximo de 20 caracteres"),
-					confirmPassword: Yup.string()
-						.required("Las contraseñas deben coincidir")
-						.oneOf(
-							[Yup.ref("password"), null],
-							"Las contraseñas deben coincidir"
-						),
+      <Formik
+        initialValues={{
+          firstName: "",
+          lastName: "",
+          email: "",
+          password: "",
+          isActive: true,
+          branchOffice: "",
+          accessLevel: "",
+          nationality: "",
+          idNumber: "",
+          phoneNumber: "",
+          driverLicenseStatus: "",
+          designatedVehicleId: "",
+        }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+        validationSchema={Yup.object({
+          firstName: Yup.string()
+            .max(20, "Debe de tener 20 caracteres o menos")
+            .required("Requerido"),
+          lastName: Yup.string()
+            .max(20, "Debe de tener 20 caracteres o menos")
+            .required("Requerido"),
+          email: Yup.string()
+            .email("El correo no tiene un formato válido")
+            .required("Requerido"),
+          password: Yup.string()
+            .required("Por favor escriba su contraseña")
+            .min(8, "Debe contener al menos 8 caracteres")
+            .max(20, "Debe contener un maximo de 20 caracteres"),
+          confirmPassword: Yup.string()
+            .required("Las contraseñas deben coincidir")
+            .oneOf(
+              [Yup.ref("password"), null],
+              "Las contraseñas deben coincidir"
+            ),
 
-					isActive: Yup.boolean().required("Requerido"),
-					branchOffice: Yup.string().required("Requerido"),
-					accessLevel: Yup.string().required("Requerido"),
-					nationality: Yup.string().required("Requerido"),
-					idNumber: Yup.number().required("Requerido"),
-					phoneNumber: Yup.number("Escriba Numero").required("Requerido"),
-					designatedVehicleId: Yup.string(),
-				})}
-			>
-				{(Formik) => (
-					<Form>
-						<CustomInput
-							label="Nombre"
-							name="firstName"
-							placeholder="Nombre"
-							allowedCharacters="justLetters"
-						/>
-						<CustomInput
-							label="Apellido"
-							name="lastName"
-							placeholder="Apellido"
-						/>
-						<CustomInput
-							label="Correo Electronico"
-							name="email"
-							placeholder="email@google.com"
-							type="email"
-						/>
-						<CustomInput
-							label="Contraseña"
-							name="password"
-							placeholder="Contraseña"
-							type="password"
-						/>
-						<CustomInput
-							label="Confirmar Contraseña"
-							name="confirmPassword"
-							placeholder="Confirmar Contraseña"
-							type="password"
-						/>
-						{/* SELECT */}
-						<CustomInput
-							label="Sucursal"
-							name="branchOffice"
-							placeholder="Sucursal"
-						/>
-						<CustomInput
-							label="Nacionalidad"
-							name="nationality"
-							placeholder="Nacionalidad"
-						/>
-						<CustomInput
-							label="Cedula"
-							name="idNumber"
-							placeholder="Cedula"
-							allowedCharacters="positiveIntegers"
-						/>
+          isActive: Yup.boolean().required("Requerido"),
+          branchOffice: Yup.string().required("Requerido"),
+          accessLevel: Yup.string().required("Requerido"),
+          nationality: Yup.string().required("Requerido"),
+          idNumber: Yup.number().required("Requerido"),
+          phoneNumber: Yup.number("Escriba Numero").required("Requerido"),
+          designatedVehicleId: Yup.string(),
+        })}
+      >
+        {(Formik) => (
+          <Form>
+            <CustomInput
+              label="Nombres"
+              name="firstName"
+              placeholder="Nombre"
+              allowedcharacters="justLetters"
+              maxLength={25}
+            />
+            <CustomInput
+              label="Apellidos"
+              name="lastName"
+              placeholder="Apellido"
+              allowedcharacters="justLetters"
+              maxLength={25}
+            />
+            <CustomInput
+              label="Correo Electronico"
+              name="email"
+              placeholder="email@google.com"
+              type="email"
+            />
+            <CustomInput
+              label="Contraseña"
+              name="password"
+              placeholder="Contraseña"
+              type="password"
+              maxLength={25}
+            />
+            <CustomInput
+              label="Confirmar Contraseña"
+              name="confirmPassword"
+              placeholder="Confirmar Contraseña"
+              type="password"
+              maxLength={25}
+            />
+            {/* SELECT */}
+            <CustomSelect label="Sucursal" name="branchOffice">
+              <option value="">Seleccionar Sucursal</option>
+              {/* OPCIONES CARGADAS DESDE EL SERVIDOR */}
+            </CustomSelect>
 
-						<CustomInput
-							label="Telefono"
-							name="phoneNumber"
-							placeholder="Telefono"
-						/>
-						{/* <CustomInput
+            <CustomInput
+              label="Nacionalidad"
+              name="nationality"
+              placeholder="Nacionalidad"
+              allowedcharacters="justLetters"
+              maxLength={15}
+            />
+            <CustomInput
+              label="Cedula"
+              name="idNumber"
+              placeholder="Cedula"
+              maxLength={8}
+              allowedcharacters="positiveIntegers"
+            />
+
+            <CustomInput
+              label="Telefono"
+              name="phoneNumber"
+              placeholder="Telefono"
+              maxLength={13}
+              allowedcharacters="positiveIntegers"
+            />
+
+            <CustomSelect label="Cargo" name="accesLevel">
+              <option value="">Elija una Opción</option>
+              <option value="Gerente de Sucursal">Gerente de Sucursal</option>
+              <option value="Operador de Despacho">Operador de Despacho</option>
+              <option value="Conductor">Conductor</option>
+            </CustomSelect>
+
+            {/* <CustomInput
               label="Estado de la Licencia"
               name="driverLicenseStatus"
               placeholder="Estado de la Licencia"
               type="driverLicenseStatus"
             /> */}
-
-						<CustomSelect label="Cargo" name="accesLevel">
-							<option value="">Elija una Opción</option>
-							<option value="Gerente de Sucursal">Gerente de Sucursal</option>
-							<option value="Conductor">Conductor</option>
-							<option value="Operador de Despacho">Operador de Despacho</option>
-						</CustomSelect>
-						<CustomInput
-							label="Numero de Licencia"
-							name="designatedVehicleId"
-							placeholder="Numero de Licencia"
-						/>
-						<button type="submit">Enviar</button>
-					</Form>
-				)}
-			</Formik>
-		</div>
-	);
+            <CustomInput
+              label="Numero de Licencia"
+              name="designatedVehicleId"
+              placeholder="Numero de Licencia"
+              allowedcharacters="positiveIntegers"
+              maxLength={15}
+            />
+            <button type="submit">Enviar</button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
 };
--- src/components/shared/CustomInput.jsx
+++ src/components/shared/CustomInput.jsx
@@ -2,66 +2,66 @@ import { ErrorMessage, useField } from "formik";
 import { useState } from "react";
 
 export const CustomInput = ({ label, ...props }) => {
-	const [field, , helper] = useField(props);
+  const [field, , helper] = useField(props);
 
-	const handleInputChange = (event) => {
-		// Disable default event handler
-		event.preventDefault();
+  const handleInputChange = (event) => {
+    // Disable default event handler
+    event.preventDefault();
 
-		// Extract current Input value
-		const { value } = event.target;
+    // Extract current Input value
+    const { value } = event.target;
 
-		// Display current Input value
-		// console.log(value);
+    // Display current Input value
+    // console.log(value);
 
-		// VALIDATION - Verifying for character restrictions
-		if (props.allowedCharacters) {
-			// Verifying if the field value changed
+    // VALIDATION - Verifying for character restrictions
+    if (props.allowedcharacters) {
+      // Verifying if the field value changed
 
-			switch (props.allowedCharacters) {
-				// CASE: Positive integers only
-				case "positiveIntegers": {
-					const regex = /^[1-9]\d*$/;
-					if (regex.test(value.toString()) || value === "") {
-						helper.setValue(value);
-					}
-					break;
-				}
+      switch (props.allowedcharacters) {
+        // CASE: Positive integers only
+        case "positiveIntegers": {
+          const regex = /^[1-9]\d*$/;
+          if (regex.test(value.toString()) || value === "") {
+            helper.setValue(value);
+          }
+          break;
+        }
+        // CASE: Letters only
+        case "justLetters": {
+          const regex = /^[a-zA-ZáéíóúüÁÉÍÓÚÜñÑ\s]+$/;
+          if (regex.test(value.toString()) || value === "") {
+            helper.setValue(value);
+            console.log(value);
+          }
 
-				// CASE: Letters only
-				case "justLetters": {
-					const regex = /[\p{Letter}\s]+/gu;
-					if (regex.test(value.toString()) || value === "") {
-						helper.setValue(value);
-					}
+          break;
+        }
+        default: {
+          break;
+        }
+      }
+    } else {
+      // Default behavior when the 'allowedcharacters' prop is not received
+      helper.setValue(value);
+    }
+  };
 
-					break;
-				}
-				default: {
-					break;
-				}
-			}
-		} else {
-			// Default behavior when the 'allowedCharacters' prop is not received
-			helper.setValue(value);
-		}
-	};
-
-	return (
-		<>
-			<label htmlFor={props.id || props.name}>{label}</label>
-			<input
-				className="text-input"
-				{...field}
-				{...props}
-				// value={userInput}
-				onChange={handleInputChange}
-			/>
-			<ErrorMessage
-				name={props.name}
-				component="span"
-				className="custon-span-error-class"
-			/>
-		</>
-	);
+  return (
+    <>
+      <label htmlFor={props.id || props.name}>{label}</label>
+      <input
+        className="text-input"
+        {...field}
+        {...props}
+        // value={userInput}
+        onChange={handleInputChange}
+      />
+      <ErrorMessage
+        name={props.name}
+        component="span"
+        className="custon-span-error-class"
+      />
+    </>
+  );
 };
