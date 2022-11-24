deaae0738048c881e1bc619e8de008fb911537f3
Enrique Gimeno <enriquejgimeno@gmail.com>
2022-11-24 13:10:31 -0300

    Add NavBar to App.js

--- src/App.js
+++ src/App.js
@@ -6,17 +6,46 @@ import { CreateClient } from "./components/feature/forms/CreateClient";
 import { CreateDestination } from "./components/feature/forms/CreateDestination";
 import { CreateProduct } from "./components/feature/forms/CreateProduct";
 import { CreateVehicleModel } from "./components/feature/forms/CreateVehicleModel";
+import { Navigate, NavLink, Route, Routes } from "react-router-dom";
 
 function App() {
+  let activeClassName = "nav-active";
   return (
     <div className="App">
-      {/* <Login />
-      <AdminPanel /> */}
-      {/* <CreateUser /> */}
-      {/* <CreateClient /> */}
-      {/* <CreateDestination /> */}
-      {/* <CreateProduct /> */}
-      <CreateVehicleModel />
+      <div className="app-menu">
+        <header>{/* <h1 className="App-header">COMPONENTS</h1> */}</header>
+        <nav>
+          <NavLink
+            to=""
+            className={
+              ("nav-link ", ({ isActive }) => (isActive ? activeClassName : ""))
+            }
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to="login"
+            className={
+              ("nav-link ", ({ isActive }) => (isActive ? activeClassName : ""))
+            }
+          >
+            Login
+          </NavLink>
+          <NavLink
+            to="admin-panel"
+            className={
+              ("nav-link ", ({ isActive }) => (isActive ? activeClassName : ""))
+            }
+          >
+            Admin{" "}
+          </NavLink>
+        </nav>
+      </div>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/admin-panel" element={<AdminPanel />} />
+        <Route path="/*" element={<Navigate to="/" />} />
+      </Routes>
     </div>
   );
 }
