8250852f651623a944385723b836ef3dd2b7b0ea
Enrique Gimeno <enriquejgimeno@gmail.com>
2022-11-25 00:15:42 -0300

    Add create forms export to index.js in forms

---
+++ src/components/feature/forms/index.js
@@ -0,0 +1,8 @@
+export { CreateBranchOffice } from "./CreateBranchOffice";
+export { CreateClient } from "./CreateClient";
+export { CreateDestination } from "./CreateDestination";
+export { CreateOrder } from "./CreateOrder";
+export { CreateProduct } from "./CreateProduct";
+export { CreateUser } from "./CreateUser";
+export { CreateVehicle } from "./CreateVehicle";
+export { CreateVehicleModel } from "./CreateVehicleModel";
