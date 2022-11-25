660c927160ac11d1a11d2206c20700dfc8f09eca
Enrique Gimeno <enriquejgimeno@gmail.com>
2022-11-25 00:21:03 -0300

    Add class and styles to SideBar container

--- src/components/shared/Sidebar/Sidebar.jsx
+++ src/components/shared/Sidebar/Sidebar.jsx
@@ -2,5 +2,5 @@ import React from "react";
 import "./Sidebar.css";
 
 export default function Sidebar() {
-  return <div>Sidebar</div>;
+  return <div className="sidebar-container">Sidebar</div>;
 }
