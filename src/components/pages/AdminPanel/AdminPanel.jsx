0f40c469da93ec36ba47f54ebd94b96a3fa76562
Enrique Gimeno <enriquejgimeno@gmail.com>
2022-11-25 00:22:37 -0300

    Add child components to AdminPanel

--- src/components/pages/AdminPanel/AdminPanel.jsx
+++ src/components/pages/AdminPanel/AdminPanel.jsx
@@ -1,6 +1,17 @@
 import React from "react";
+import { FormContainer } from "../../feature/FormContainer/FormContainer";
+import Sidebar from "../../shared/Sidebar/Sidebar";
+import { TopNavBar } from "../../shared/TopNavBar/TopNavBar";
 import "./AdminPanel.css";
 
 export default function AdminPanel() {
-  return <div>AdminPanel</div>;
+  return (
+    <div className="panel-container">
+      <TopNavBar />
+      <div className="bottom-panel">
+        <Sidebar />
+        <FormContainer />
+      </div>
+    </div>
+  );
 }
