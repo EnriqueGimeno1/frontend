866def8f64246addb7fba3e6ffc4c669c6764ad0
Luis Martinez <ernestox11@hotmail.com>
2022-11-26 11:42:53 -0400

    Add logo to TopNavBar

--- src/components/shared/TopNavBar/TopNavBar.jsx
+++ src/components/shared/TopNavBar/TopNavBar.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import "./TopNavBar.css";
+import logo from "../../../assets/images/logo.png";
 
 export const TopNavBar = () => {
-	return <div className="top-bar-container"></div>;
+	return (
+		<div className="top-bar-container">
+			<img src={logo} className="nav-logo" alt="Logo" />
+		</div>
+	);
 };
