fed203f6f561aa2eb3ec63c3d96210a5ed33c206
Luis Martinez <ernestox11@hotmail.com>
2022-11-26 08:26:05 -0400

    Disable unused imports in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
 import Login from "./components/pages/Login/Login";
 import AdminPanel from "./components/pages/AdminPanel/AdminPanel";
-import { CreateUser } from "./components/feature/forms/CreateUser";
-import { CreateClient } from "./components/feature/forms/CreateClient";
-import { CreateDestination } from "./components/feature/forms/CreateDestination";
-import { CreateProduct } from "./components/feature/forms/CreateProduct";
-import { CreateVehicleModel } from "./components/feature/forms/CreateVehicleModel";
+// import { CreateUser } from "./components/feature/forms/CreateUser";
+// import { CreateClient } from "./components/feature/forms/CreateClient";
+// import { CreateDestination } from "./components/feature/forms/CreateDestination";
+// import { CreateProduct } from "./components/feature/forms/CreateProduct";
+// import { CreateVehicleModel } from "./components/feature/forms/CreateVehicleModel";
 import { Navigate, NavLink, Route, Routes } from "react-router-dom";
 
 function App() {
