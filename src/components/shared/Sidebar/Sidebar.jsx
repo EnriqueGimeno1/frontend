9db9e0727bbab773c7a5e5872f915344b759b900
Luis Martinez <ernestox11@hotmail.com>
2022-11-26 12:15:39 -0400

    Add placeholder NavLinks in Sidebar component

--- src/components/shared/Sidebar/Sidebar.jsx
+++ src/components/shared/Sidebar/Sidebar.jsx
@@ -1,6 +1,39 @@
 import React from "react";
 import "./Sidebar.css";
+import { NavLink } from "react-router-dom";
 
 export default function Sidebar() {
-	return <div className="sidebar-container"></div>;
+	let activeClassName = "side-nav-active";
+
+	return (
+		<div className="sidebar-container">
+			{/* Load links based on user's access level */}
+			<nav className="nav-container">
+				<NavLink
+					className={({ isActive }) =>
+						isActive ? activeClassName : "side-nav-link"
+					}
+					to="/"
+				>
+					Registrar Usuario
+				</NavLink>
+				<NavLink
+					className={({ isActive }) =>
+						isActive ? activeClassName : "side-nav-link"
+					}
+					to="/about"
+				>
+					Registrar Vehículo
+				</NavLink>
+				<NavLink
+					className={({ isActive }) =>
+						isActive ? activeClassName : "side-nav-link"
+					}
+					to="/about"
+				>
+					Asignar Ruta
+				</NavLink>
+			</nav>
+		</div>
+	);
 }
