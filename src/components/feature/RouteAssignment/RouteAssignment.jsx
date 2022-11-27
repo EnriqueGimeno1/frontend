8cb190ea07ba074af26f30fdad0ed2c8a61d6260
Enrique Gimeno <enriquejgimeno@gmail.com>
2022-11-27 11:52:32 -0300

    Create RouteAssignments component

---
+++ src/components/feature/RouteAssignment/RouteAssignment.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./RouteAssignment.css";
+import { SelectionList } from "../../shared/SelectionList/SelectionList";
+
+export const RouteAssignment = () => {
+  return (
+    <div className="assignment-container">
+      <SelectionList />
+    </div>
+  );
+};
