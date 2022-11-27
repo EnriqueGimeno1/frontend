139b544b4b52b001f367b0281a86b7751b0f3e1f
Enrique Gimeno <enriquejgimeno@gmail.com>
2022-11-27 17:22:14 -0300

    Import ListElement in SelectionList

--- src/components/shared/SelectionList/SelectionList.jsx
+++ src/components/shared/SelectionList/SelectionList.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { ListElement } from "../ListElement/ListElement";
 import "./SelectionList.css";
 
 export const SelectionList = () => {
@@ -6,6 +7,16 @@ export const SelectionList = () => {
     <div className="selection-list-container">
       <h1 className="title">Conductores</h1>
       <div className="list-container">
+        <div className="list-element"></div>
+        <div className="list-element"></div>
+        <div className="list-element"></div>
+        <div className="list-element"></div>
+        <div className="list-element"></div>
+        <div className="list-element"></div>
+        <div className="list-element"></div>
+        <div className="list-element"></div>
+        <div className="list-element"></div>
+        <div className="list-element"></div>
         <div className="list-element"></div>
         <div className="list-element"></div>
         <div className="list-element"></div>
