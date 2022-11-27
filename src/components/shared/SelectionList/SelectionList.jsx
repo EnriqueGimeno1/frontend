eb5628793d4caae6ca80293c3ed6f2cecbcd81ea
Enrique Gimeno <enriquejgimeno@gmail.com>
2022-11-27 11:51:44 -0300

    Create SelectionList component

---
+++ src/components/shared/SelectionList/SelectionList.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./SelectionList.css";
+
+export const SelectionList = () => {
+  return (
+    <div className="selection-list-container">
+      <h1 className="title">Conductores</h1>
+      <div className="list-container">
+        <div className="list-element"></div>
+        <div className="list-element"></div>
+        <div className="list-element"></div>
+      </div>
+    </div>
+  );
+};
