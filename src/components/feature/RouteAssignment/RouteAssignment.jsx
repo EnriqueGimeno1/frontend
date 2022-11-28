3ae0d15aa6ca29c5617626bed982c01f21403c55
Luis Martinez <ernestox11@hotmail.com>
2022-11-28 15:24:18 -0400

    Fix style conflict in RouteAssignment

--- src/components/feature/RouteAssignment/RouteAssignment.jsx
+++ src/components/feature/RouteAssignment/RouteAssignment.jsx
@@ -1,26 +1,29 @@
 import React from "react";
 import "./RouteAssignment.css";
 import { SelectionList } from "../../shared/SelectionList/SelectionList";
+import { LoadBar } from "../LoadBar/LoadBar";
 
 export const RouteAssignment = () => {
-  return (
-    <div className="assignment-container">
-      <div className="selection-panel-container">
-        <SelectionList />
-        <SelectionList />
-        <SelectionList />
-      </div>
-      <div className="load-bar-container">Barra de carga</div>
-      <div className="bottom-panel">
-        <div className="details-container">
-          <span className="text1">text 1</span>
-          <span className="text2">text 1</span>
-        </div>
-        <div className="action-container">
-          <button className="button button1" />
-          <button className="button button2" />
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="assignment-container">
+			<div className="selection-panel-container">
+				<SelectionList />
+				<SelectionList />
+				<SelectionList />
+			</div>
+			<div className="load-bar-container">
+				<LoadBar />
+			</div>
+			<div className="bottom-selection-panel">
+				<div className="details-container">
+					<span className="text1">text 1</span>
+					<span className="text2">text 1</span>
+				</div>
+				<div className="action-container">
+					<button className="button button1" />
+					<button className="button button2" />
+				</div>
+			</div>
+		</div>
+	);
 };
