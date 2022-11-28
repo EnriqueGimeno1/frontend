bf747ecd877715ffc6881e97dd95e3349e3f313c
Enrique Gimeno <enriquejgimeno@gmail.com>
2022-11-28 14:25:00 -0300

    Setup ListElement component

--- src/components/shared/ListElement/ListElement.jsx
+++ src/components/shared/ListElement/ListElement.jsx
@@ -4,14 +4,17 @@ import "./ListElement.css";
 export const ListElement = () => {
   return (
     <div className="list-element-container">
-      <div className="picture container">
-        <img src="" alt="Foto Usuario" />
+      <div className="picture-container">
+        <img
+          className="picture"
+          src="https://media.istockphoto.com/photos/english-bulldog-in-a-pumpkin-wagon-picture-id1280623255?b=1&k=20&m=1280623255&s=612x612&w=0&h=KzVaFbeq9HJdQZdGPXNd5xbfPUbKsjvhZdNC6Ii92pM="
+          alt="Foto Usuario"
+        />
       </div>
       <div className="info-container">
-        <div className="element-info">
-          <span>Enrique Gimeno</span>
-          <span>Tiene sueño</span>
-        </div>
+        <span className="element-title">Enrique Gimeno</span>
+        <span className="element-text">Tiene sueño</span>
+        <span className="element-text2">Tiene sueño</span>
       </div>
     </div>
   );
