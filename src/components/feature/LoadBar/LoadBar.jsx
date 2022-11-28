493845480cc9da7d84a4491c0458407554538a81
Enrique Gimeno <enriquejgimeno@gmail.com>
2022-11-28 20:25:36 -0300

    Replace width value with a dynamic value

--- src/components/feature/LoadBar/LoadBar.jsx
+++ src/components/feature/LoadBar/LoadBar.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import "./LoadBar.css";
 
 export const LoadBar = () => {
-	return (
-		<div className="load-bar-container">
-			<div className="progress-bar" style={{ width: "30%" }}></div>
-		</div>
-	);
+  const currentProgress = "10%";
+  return (
+    <div className="load-bar-container">
+      <div className="progress-bar" style={{ width: currentProgress }}></div>
+    </div>
+  );
 };
