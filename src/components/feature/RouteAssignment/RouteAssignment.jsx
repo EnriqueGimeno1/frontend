0fea1f305f7ede56ee7d3372bf0493d4e01b0a6b
Enrique Gimeno <enriquejgimeno@gmail.com>
2022-11-28 14:28:42 -0300

    Setup RouteAssignment component

--- src/components/feature/RouteAssignment/RouteAssignment.jsx
+++ src/components/feature/RouteAssignment/RouteAssignment.jsx
@@ -5,7 +5,22 @@ import { SelectionList } from "../../shared/SelectionList/SelectionList";
 export const RouteAssignment = () => {
   return (
     <div className="assignment-container">
-      <SelectionList />
+      <div className="selection-panel-container">
+        <SelectionList />
+        <SelectionList />
+        <SelectionList />
+      </div>
+      <div className="load-bar-container">Barra de carga</div>
+      <div className="bottom-panel">
+        <div className="details-container">
+          <span className="text1">text 1</span>
+          <span className="text2">text 1</span>
+        </div>
+        <div className="action-container">
+          <button className="button button1" />
+          <button className="button button2" />
+        </div>
+      </div>
     </div>
   );
 };
