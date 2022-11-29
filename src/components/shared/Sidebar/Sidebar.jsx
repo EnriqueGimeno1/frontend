a4e54ee3713e28c4160ca06ac3fc2c37edb2c348
Luis Martinez <ernestox11@hotmail.com>
2022-11-29 10:06:09 -0400

    Adjust UserProfile shadow overflowing sidebar

--- src/components/shared/Sidebar/Sidebar.jsx
+++ src/components/shared/Sidebar/Sidebar.jsx
@@ -4,28 +4,30 @@ import { NavLink } from "react-router-dom";
 import { UserProfile } from "../UserProfile/UserProfile";
 
 export default function Sidebar({ userActions }) {
-  let activeClassName = "side-nav-active";
+	let activeClassName = "side-nav-active";
 
-  return (
-    <div className="sidebar-container">
-      {/* {JSON.stringify(userActions)} */}
-      {/* Load links based on user's access level */}
-      <nav className="nav-container">
-        {userActions.map((userAction, index) => {
-          return (
-            <NavLink
-              key={index}
-              className={({ isActive }) =>
-                isActive ? activeClassName : "side-nav-link"
-              }
-              to={userAction.path}
-            >
-              {userAction.text}
-            </NavLink>
-          );
-        })}
-      </nav>
-      <UserProfile />
-    </div>
-  );
+	return (
+		<div className="sidebar-container">
+			{/* {JSON.stringify(userActions)} */}
+			{/* Load links based on user's access level */}
+			<nav className="nav-container">
+				{userActions.map((userAction, index) => {
+					return (
+						<NavLink
+							key={index}
+							className={({ isActive }) =>
+								isActive ? activeClassName : "side-nav-link"
+							}
+							to={userAction.path}
+						>
+							{userAction.text}
+						</NavLink>
+					);
+				})}
+			</nav>
+			<div className="profile-info">
+				<UserProfile />
+			</div>
+		</div>
+	);
 }
