ed277f3a7d974fc88ea6e6f11905fe8aedc02fd3
Enrique Gimeno <enriquejgimeno@gmail.com>
2022-11-30 14:15:00 -0300

    Add ListElement props

--- src/components/shared/ListElement/ListElement.jsx
+++ src/components/shared/ListElement/ListElement.jsx
@@ -1,20 +1,16 @@
 import React from "react";
 import "./ListElement.css";
 
-export const ListElement = () => {
+export const ListElement = ({ imageSrc, title, text1, text2 }) => {
   return (
     <div className="list-element-container">
       <div className="picture-container">
-        <img
-          className="picture"
-          src="https://media.istockphoto.com/photos/english-bulldog-in-a-pumpkin-wagon-picture-id1280623255?b=1&k=20&m=1280623255&s=612x612&w=0&h=KzVaFbeq9HJdQZdGPXNd5xbfPUbKsjvhZdNC6Ii92pM="
-          alt="Foto Usuario"
-        />
+        <img className="picture" src={imageSrc} alt="Miniatura" />
       </div>
       <div className="info-container">
-        <span className="element-title">Enrique Gimeno</span>
-        <span className="element-text">Tiene sueño</span>
-        <span className="element-text2">Tiene sueño</span>
+        <span className="element-title">{title}</span>
+        <span className="element-text">{text1}</span>
+        <span className="element-text2">{text2}</span>
       </div>
     </div>
   );
