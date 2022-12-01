86106e321aca80f1f85b66079ab7dfd6e1bbc16d
Luis Martinez <ernestox11@hotmail.com>
2022-12-01 16:27:43 -0400

    Change displayed props in ListElement

--- src/components/shared/ListElement/ListElement.jsx
+++ src/components/shared/ListElement/ListElement.jsx
@@ -8,9 +8,9 @@ export const ListElement = ({ props }) => {
 				<img className="picture" src={props.imageSrc} alt="Miniatura" />
 			</div>
 			<div className="info-container">
-				<span className="element-title">{props.title}</span>
-				<span className="element-text">{props.text1}</span>
-				<span className="element-text2">{props.text2}</span>
+				<span className="element-title">{props.firstName}</span>
+				<span className="element-text">{props.idNumber}</span>
+				<span className="element-text2">{props.phoneNumber}</span>
 			</div>
 		</div>
 	);
