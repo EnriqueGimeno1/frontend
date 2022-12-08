acdf5298982663cec8b84691ac21a08c89c9bd47
Luis Martinez <ernestox11@hotmail.com>
2022-12-08 14:43:45 -0400

    Add new route to AdminPanel

--- src/components/pages/AdminPanel/AdminPanel.jsx
+++ src/components/pages/AdminPanel/AdminPanel.jsx
@@ -5,23 +5,27 @@ import { TopNavBar } from "../../shared/TopNavBar/TopNavBar";
 import "./AdminPanel.css";
 
 export default function AdminPanel() {
-  const userActions = [
-    {
-      text: "Sucursales",
-      path: "/create-branch-office",
-    },
-    {
-      text: "Usuarios",
-      path: "/create-destination",
-    },
-  ];
-  return (
-    <div className="panel-container">
-      <TopNavBar />
-      <div className="bottom-panel">
-        <Sidebar userActions={userActions} />
-        <FormContainer />
-      </div>
-    </div>
-  );
+	const userActions = [
+		{
+			text: "Sucursales",
+			path: "create-branch-office",
+		},
+		{
+			text: "Usuarios",
+			path: "create-destination",
+		},
+		{
+			text: "Rutas",
+			path: "route-assignment",
+		},
+	];
+	return (
+		<div className="panel-container">
+			<TopNavBar />
+			<div className="bottom-panel">
+				<Sidebar userActions={userActions} />
+				<FormContainer />
+			</div>
+		</div>
+	);
 }
