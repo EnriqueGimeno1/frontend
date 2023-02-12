6223a3c38a91a7870cacf82732cb3838bdd36b7d
Luis Martinez <ernestox11@hotmail.com>
2023-02-12 11:46:43 -0400

    Update LoadBar styles

--- src/components/feature/LoadBar/LoadBar.jsx
+++ src/components/feature/LoadBar/LoadBar.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import "./LoadBar.css";
 
 export const LoadBar = () => {
-  const currentProgress = "10%";
-  return (
-    <div className="load-bar-container">
-      <div className="progress-bar" style={{ width: currentProgress }}></div>
-    </div>
-  );
+	const currentProgress = "90%";
+	return (
+		<div className="load-bar-container">
+			<div className="progress-bar" style={{ width: currentProgress }}></div>
+		</div>
+	);
 };
