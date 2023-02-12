88bea1fa806dbcee115812d886dc080c98365ebe
Luis Martinez <ernestox11@hotmail.com>
2023-02-12 11:48:12 -0400

    Implement basic authentication

--- src/App.js
+++ src/App.js
@@ -8,11 +8,24 @@ import AdminPanel from "./components/pages/AdminPanel/AdminPanel";
 // import { CreateVehicleModel } from "./components/feature/forms/CreateVehicleModel";
 import { Navigate, NavLink, Route, Routes } from "react-router-dom";
 import { RouteAssignment } from "./components/feature/RouteAssignment/RouteAssignment";
+import { useState } from "react";
 
 function App() {
 	let activeClassName = "nav-active";
+	const [authenticatedUser, setAuthenticatedUser] = useState();
+
+	const props = { authenticatedUser, setAuthenticatedUser };
 	return (
 		<div className="App">
+			<Routes>
+				<Route index element={<Login {...props} />} />
+				<Route path="admin-panel" element={<AdminPanel />}>
+					<Route path="route-assignment" element={<RouteAssignment />} />
+				</Route>
+				<Route path="/*" element={<Navigate to="/" />} />
+			</Routes>
+
+			{/* TEMPORAL FLOATING MENU */}
 			<div className="app-menu">
 				<header>{/* <h1 className="App-header">COMPONENTS</h1> */}</header>
 				<nav>
@@ -50,14 +63,6 @@ function App() {
 					</NavLink>
 				</nav>
 			</div>
-			<Routes>
-				<Route index element={<Login />} />
-				<Route path="admin-panel" element={<AdminPanel />}>
-					<Route path="route-assignment" element={<RouteAssignment />} />
-				</Route>
-
-				<Route path="/*" element={<Navigate to="/" />} />
-			</Routes>
 		</div>
 	);
 }
--- src/components/pages/Login/Login.jsx
+++ src/components/pages/Login/Login.jsx
@@ -1,8 +1,89 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 import "./Login.css";
 import logo from "../../../assets/images/logo.png";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
-export default function Login() {
+export default function Login({ authenticatedUser, setAuthenticatedUser }) {
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+	const navigate = useNavigate();
+
+	const handleUsernameChange = useCallback((event) => {
+		event.preventDefault();
+		setUsername(event.target.value);
+		// console.log(username);
+	}, []);
+
+	const handlePasswordChange = useCallback((event) => {
+		event.preventDefault();
+		setPassword(event.target.value);
+		// console.log(event.target.value);
+	}, []);
+
+	const handleLogin = useCallback(
+		async (event) => {
+			event.preventDefault();
+			await axios({
+				method: "post",
+				url: "http://localhost:3333/users/login",
+				data: {
+					username: username,
+					password: password,
+				},
+				// responseType: "json",
+			})
+				.then(function (response) {
+					// console.log(response.data);
+					console.log(response.data.User.accessLevel);
+					setAuthenticatedUser(response.data);
+					// Check if user access level is retrieved
+					if (typeof response.data.User.accessLevel !== "undefined") {
+						console.log("hello");
+						switch (response.data.User.accessLevel) {
+							case "Administrador de Sistema":
+								console.log("Administrador de Sistema");
+								navigate("/admin-panel");
+								break;
+							case "Gerente de Sucursal":
+								console.log("Gerente de Sucursal");
+								navigate("/admin-panel");
+								break;
+							case "Operador de Transporte":
+								console.log("Operador de Transporte");
+								navigate("/admin-panel");
+								break;
+							case "Conductor":
+								console.log("Conductor");
+								navigate("/admin-panel");
+								break;
+							default:
+								break;
+						}
+					}
+				})
+				.catch(function (error) {
+					if (error.response) {
+						// The request was made and the server responded with a status code
+						// that falls out of the range of 2xx
+						console.log(error.response.data);
+						console.log(error.response.status);
+						console.log(error.response.headers);
+					} else if (error.request) {
+						// The request was made but no response was received
+						// `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+						// http.ClientRequest in node.js
+						console.log(error.request);
+					} else {
+						// Something happened in setting up the request that triggered an Error
+						console.log("Error", error.message);
+					}
+					console.log(error.config);
+				});
+		},
+		[navigate, password, setAuthenticatedUser, username]
+	);
+
 	return (
 		<div className="container">
 			<div className="login-container">
@@ -12,7 +93,7 @@ export default function Login() {
 					className="logo"
 					alt="Logo"
 				/>
-				<form action="" className="login-form">
+				<form action="" className="login-form" onSubmit={handleLogin}>
 					<label htmlFor="user" className="form-label">
 						Usuario
 					</label>
@@ -20,7 +101,9 @@ export default function Login() {
 						type="text"
 						className="form-input"
 						name="User"
-						placeholder="Usuario"
+						value={username}
+						onChange={handleUsernameChange}
+						// placeholder="Usuario"
 					/>
 					<label htmlFor="password" className="form-label">
 						Contraseña
@@ -29,7 +112,9 @@ export default function Login() {
 						type="password"
 						className="form-input"
 						name="password"
-						placeholder="Contraseña"
+						value={password}
+						onChange={handlePasswordChange}
+						// placeholder="Contraseña"
 					/>
 					<input className="submit-button" type="submit" value="Ingresar" />
 				</form>
