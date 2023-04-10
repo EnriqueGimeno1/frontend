f2f6b284d6b08069e603817c65de5bf8fea542e0
Luis <ernestox11@gmail.com>
2023-04-10 18:00:25 -0400

    Add createVehicle component

--- src/components/feature/forms/CreateVehicle.jsx
+++ src/components/feature/forms/CreateVehicle.jsx
@@ -5,42 +5,42 @@ import "./FormsStyles.css";
 import { CustomSelect, CustomInput } from "../../shared";
 
 export const CreateVehicle = () => {
-  return (
-    <div>
-      <h1>Create Vehicle</h1>
+	return (
+		<div>
+			<h1>Create Vehicle</h1>
 
-      <Formik
-        initialValues={{
-          status: "",
-          branchOfficeId: "",
-          capacity: "",
-          model: "",
-        }}
-        onSubmit={(values) => {
-          console.log(values);
-        }}
-        validationSchema={Yup.object({
-          status: Yup.string("Caracteres Incorrectos")
-            .max(15, "Debe de tener 15 caracteres o menos")
-            .required("Requerido"),
-        })}
-      >
-        {(Formik) => (
-          <Form>
-            <CustomSelect label="Sucursal" name="branchOfficeId">
-              <option value="">Seleccione Marca</option>
-            </CustomSelect>
+			<Formik
+				initialValues={{
+					status: "",
+					branchOfficeId: "",
+					capacity: "",
+					model: "",
+				}}
+				onSubmit={(values) => {
+					console.log(values);
+				}}
+				validationSchema={Yup.object({
+					status: Yup.string("Caracteres Incorrectos")
+						.max(15, "Debe de tener 15 caracteres o menos")
+						.required("Requerido"),
+				})}
+			>
+				{(Formik) => (
+					<Form>
+						<CustomSelect label="Sucursal" name="branchOfficeId">
+							<option value="">Seleccione Marca</option>
+						</CustomSelect>
 
-            <CustomInput
-              label="Capacidad"
-              name="capacity"
-              placeholder="Capacidad"
-            />
-            <CustomInput label="Modelo" name="model" placeholder="Modelo" />
-            <button type="submit">Registrar</button>
-          </Form>
-        )}
-      </Formik>
-    </div>
-  );
+						<CustomInput
+							label="Capacidad"
+							name="capacity"
+							placeholder="Capacidad"
+						/>
+						<CustomInput label="Modelo" name="model" placeholder="Modelo" />
+						<button type="submit">Registrar</button>
+					</Form>
+				)}
+			</Formik>
+		</div>
+	);
 };
