ea8133d85750767c5c8de3f5502e80cf69ae2903
Luis <ernestox11@gmail.com>
2023-04-13 06:50:56 -0400

    Update displayed driver information in RouteAssignment panel

--- src/components/feature/listElements/DriverListElement/DriverListElement.jsx
+++ src/components/feature/listElements/DriverListElement/DriverListElement.jsx
@@ -4,44 +4,46 @@ import { useCallback } from "react";
 import { useState } from "react";
 
 export const DriverListElement = ({
-	props: {
-		element,
-		driversInfo,
-		setDriversInfo,
-		checkedDriver,
-		setCheckedDriver,
-	},
+  props: {
+    element,
+    driversInfo,
+    setDriversInfo,
+    checkedDriver,
+    setCheckedDriver,
+  },
 }) => {
-	const selectDriver = useCallback(() => {
-		const updatedDriversInfo = driversInfo.map((driver) => {
-			if (driver.idNumber === element.idNumber) {
-				return { ...driver, checked: true };
-			} else {
-				return { ...driver, checked: false };
-			}
-		});
-		setDriversInfo(updatedDriversInfo);
-		setCheckedDriver({ ...element });
-	}, [driversInfo, element, setCheckedDriver, setDriversInfo]);
+  const selectDriver = useCallback(() => {
+    const updatedDriversInfo = driversInfo.map((driver) => {
+      if (driver.idNumber === element.idNumber) {
+        return { ...driver, checked: true };
+      } else {
+        return { ...driver, checked: false };
+      }
+    });
+    setDriversInfo(updatedDriversInfo);
+    setCheckedDriver({ ...element });
+  }, [driversInfo, element, setCheckedDriver, setDriversInfo]);
 
-	return (
-		<div
-			className={[
-				"list-element-container",
-				element.checked ? "selected-list-element" : "",
-			]
-				.join(" ")
-				.trim()}
-			onClick={selectDriver}
-		>
-			<div className="picture-container">
-				<img className="picture" src={driverThumbnail} alt="Miniatura" />
-			</div>
-			<div className="info-container">
-				<span className="element-title">{element.firstName}</span>
-				<span className="element-text">V-{element.idNumber}</span>
-				<span className="element-text2">{element.phoneNumber}</span>
-			</div>
-		</div>
-	);
+  return (
+    <div
+      className={[
+        "list-element-container",
+        element.checked ? "selected-list-element" : "",
+      ]
+        .join(" ")
+        .trim()}
+      onClick={selectDriver}
+    >
+      <div className="picture-container">
+        <img className="picture" src={driverThumbnail} alt="Miniatura" />
+      </div>
+      <div className="info-container">
+        <span className="element-title">{element.firstName}</span>
+        <span className="element-text">V-{element.idNumber}</span>
+        <span className="element-text2">
+          Capacidad: {element.vehicleWeight} Kg
+        </span>
+      </div>
+    </div>
+  );
 };
